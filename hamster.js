8341b7953ae406ba33c3915002f524027f4ace09
sean lewis <seanmartinlewis@gmail.com>
2016-10-10 23:25:12 -0500

    added variable dataArray

--- hamster.js
+++ hamster.js
@@ -7,8 +7,8 @@ request('http://api.giphy.com/v1/stickers/search?q=hamster&api_key=dc6zaTOxFJmzC
     var responseObject = JSON.parse(body);
     console.log(responseObject);
 
-    // 2. Set up an array variable `dataArray` that stores the array of GIF objects. HINT: You'll want to grab the value of the "data" key in the `responseObject` object.
-    // var dataArray = ???;
+    var dataArray = responseObject.data;
+
 
     // 3. Use the `map()` function to iterate through the `dataArray` array. For each `gifObject` in `dataArray`, return the GIF URL.
     // var urlArray = dataArray.map(function(gifObject){
