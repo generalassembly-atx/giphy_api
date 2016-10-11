4ad65c7714189ced71332e9953e3c744bd6a58a2
Nathan Jensby <njensby@gmail.com>
2016-10-10 21:56:45 -0500

    Added hamster URL from giphy

--- hamster.js
+++ hamster.js
@@ -2,13 +2,13 @@ var request = require('request');
 var open = require('open');
 
 // 1. Find the correct URL to get adorable hamsters from Giphy!
-request('GIPHY API GET REQUEST URL', function (error, response, body) {
+request('http://api.giphy.com/v1/gifs/search?q=hamsters&api_key=dc6zaTOxFJmzC', function (error, response, body) {
   if (!error && response.statusCode == 200) {
     var responseObject = JSON.parse(body);
     console.log(responseObject);
 
     // 2. Set up an array variable `dataArray` that stores the array of GIF objects. HINT: You'll want to grab the value of the "data" key in the `responseObject` object.
-    // var dataArray = ???;
+     var dataArray = 
 
     // 3. Use the `map()` function to iterate through the `dataArray` array. For each `gifObject` in `dataArray`, return the GIF URL.
     // var urlArray = dataArray.map(function(gifObject){
